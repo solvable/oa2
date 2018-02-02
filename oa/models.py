1d015d6de2cee9359aa40195e59219420eaffb54
solvable <tbfham18>
2018-02-01 20:13:31 -0500

    Added in gallery page and project carousel added Materializecss. its kind of badass This doesn't concern you... go away!

--- oa/models.py
+++ oa/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.conf import settings
+from PIL import Image
+import os
 # Create your models here.
 
 class Project(models.Model):
@@ -14,11 +16,29 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     modified = models.DateTimeField(auto_now=True, auto_now_add=False)
     featuredImage = models.ImageField(upload_to='photos/%Y/%m/%d', default='/static/img/project.jpg')
+    thumbImage = models.ImageField(upload_to='photos/%Y/%m/%d', default='/static/img/project.jpg', blank=True)
 
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField(max_length=50, blank=True)
+
 
     def save(self, *args, **kwargs):
+        super(Project, self).save()
+        thumbSize = (400,300)
+        featuredSize = (800,600)
         slug = slugify(self.nickname)
+
+        # resize image and save a thumbnail
+        file = self.featuredImage.path
+        thumbfile = self.thumbImage.path
+
+        thumb = Image.open(thumbfile)
+        featuredImage = Image.open(file)
+
+        featuredImage = image.resize(featuredSize, Image.ANTIALIAS)
+        thumb= image.thumbnail(thumbSize, Image.ANTIALIAS)
+        self.thumbImage.path =  thumb.save()
+        self.featuredImage.path = featuredImage.save()
+
         super(Project, self).save(*args, **kwargs)
 
     def __unicode__(self):
