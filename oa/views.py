efd9f20a7c100eccd39a120c9d04cd4193aa4804
solvable <tbfham18>
2018-02-07 19:28:14 -0500

    Added contact dropdown gallery working, project page working, stereo page setup need GFX

--- oa/views.py
+++ oa/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from django.views import generic
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, ContextMixin
+from django.views.generic.edit import FormMixin
 from django.shortcuts import get_object_or_404
 from .models import Project, ProjectImages
-
+from .forms import ContactForm
 
 # Create your views here.
-class IndexView(TemplateView):
+class ContactMixin(FormMixin):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super().form_valid(form)
+
+
+class IndexView(ContactMixin, generic.TemplateView):
     template_name = 'index.html'
 
 
@@ -16,32 +29,37 @@ class ProjectView(generic.DetailView):
     slug_url_kwarg = "proj"
 
 
+    def get_context_data(self, **kwargs):
+        context = super(ProjectView, self).get_context_data(**kwargs)
+        context['form'] = ContactMixin
+        return context
 
 
-class GalleryView(generic.ListView):
+
+class GalleryView(ContactMixin, generic.ListView):
     model = Project
     template_name = 'gallery.html'
 
 
 
-
-class SystemView(generic.TemplateView):
+class SystemView(ContactMixin, generic.TemplateView):
     template_name = 'speakers.html'
 
 
-class SecurityView(generic.TemplateView):
+class SecurityView(ContactMixin, generic.TemplateView):
     template_name = 'security.html'
 
-class RemoteStartView(generic.TemplateView):
+class RemoteStartView(ContactMixin, generic.TemplateView):
     template_name = 'remote_start.html'
 
-class NavSystemView(generic.TemplateView):
+class NavSystemView(ContactMixin, generic.TemplateView):
     template_name = 'nav_systems.html'
 
-class DetailingView(generic.TemplateView):
+class DetailingView(ContactMixin, generic.TemplateView):
     template_name='detailing.html'
 
 
-class ContactView(generic.TemplateView):
+class ContactView(ContactMixin, generic.TemplateView):
     template_name = 'contact.html'
+
 
